1da0decbf0fdd6e5db8730a9abd2a7b39886f815
Sedrak Adoyan <set.adoyan@gmail.com>
2022-10-31 20:00:44 +0400

    refactor: full transition from id to classnames. 3.1 in html-and-css.md

--- app.js
+++ app.js
@@ -8,10 +8,10 @@
 
 // Event handling, user interaction is what starts the code execution.
 
-var taskInput = document.getElementById("new-task");//Add a new task.
-var addButton = document.getElementsByTagName("button")[0];//first button
-var incompleteTaskHolder = document.getElementById("incompleteTasks");//ul of #incompleteTasks
-var completedTasksHolder = document.getElementById("completed-tasks");//completed-tasks
+var taskInput = document.querySelector(".js-input-new");//Add a new task.
+var addButton = document.querySelector(".js-add");//first button
+var incompleteTaskHolder = document.querySelector(".js-incomplete");//ul of #incompleteTasks
+var completedTasksHolder = document.querySelector(".js-completed");//completed-tasks
 
 
 //New task list item
